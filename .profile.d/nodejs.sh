e0c9100709714db6307edcf36a22556ee33754d1
Oleksii Ruban <settlers.net@gmail.com>
2017-01-29 01:42:51 +0200

    update PATH

--- .profile.d/nodejs.sh
+++ .profile.d/nodejs.sh
@@ -1,3 +1,4 @@
-export PATH="$HOME/node/node-v6.9.4-linux-x64/bin:$PATH
+echo "Run nodejs.sh"
+export PATH=$HOME/node/node-v6.9.4-linux-x64/bin:$PATH
 export NODE_HOME="$HOME/node/node-v6.9.4-linux-x64"
 export NODE_ENV=${NODE_ENV:-production}
